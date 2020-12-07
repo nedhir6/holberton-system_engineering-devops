a22491b3ebcf533f74c7d0c97da3dd2ff26e9ded
nedhir6 <nedhirb@gmail.com>
2020-12-07 20:59:46 +0100

    path

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
-file { '/tmp/holberton':
+file { 'holberton':
   ensure  => present,
+  path => '/tmp/holberton'
   owner   => 'www-data',
   group   => 'www-data',
   mode    => '0744',
-  content => 'I love Puppet',}
+  content => 'I love Puppet',
+}
