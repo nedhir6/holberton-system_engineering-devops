f34a194bee23e4b06875d616b106563121c203ff
nedhir6 <nedhirb@gmail.com>
2020-12-07 21:09:35 +0100

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# create a file in /tmp
+# creates a file in /tmp
 file { 'holberton':
-  path    => '/tmp/holberton'
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
+    ensure  => present,
+    path    => '/tmp/holberton',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
 }
