2a94942416f9e1aa2ed2f6a2d1ecce80db533ec4
nedhir6 <nedhirb@gmail.com>
2020-12-07 20:55:42 +0100

    big pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,6 @@
+file { '/tmp/holberton':
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,3 @@
+## general
+
+ This project is simply a set of tasks to familiarize with the basic level syntax which is virtually identical in newer versions of Puppet
