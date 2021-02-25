ac39e64354d2b218f36da32e8f1080769c55584a
nedhir6 <nedhirb@gmail.com>
2021-02-25 21:13:27 +0100

    fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,9 +1,5 @@
 # User limit
-exec { 'file change':
-provider => shell,
-command  => 'sudo sed -i -e "fs.file-max = 65536" /etc/sysctl.conf',
-}
-exec { 'apply the limit':
-provider => shell,
-command  => 'sudo sysctl -p',
+exec {'UserLimit':
+  command => 'sed -i "s/nofile 5/no file 4096/; s/nofile 4/nofile 1024/" /etc/security/limits.conf',
+  path    => ['/usr/bin', '/bin'],
 }
