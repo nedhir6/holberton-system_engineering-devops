1c0ceab7a97117121b05df68f3c6f896f381dbde
nedhir6 <nedhirb@gmail.com>
2021-02-25 20:53:18 +0100

    user limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+# User limit
+exec { 'file change':
+provider => 'shell',
+command  => 'sudo echo "fs.file-max = 65536" >> /etc/sysctl.conf',
+}
+exec { 'apply the limit':
+provider => 'shell',
+command  => 'sudo sysctl -p',
+}
