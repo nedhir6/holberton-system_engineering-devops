ff499cd117cd1313e44d98811a33e1d488b110f9
nedhir6 <nedhirb@gmail.com>
2021-03-03 18:44:56 +0100

    pep8 fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """queries the Reddit API and returns the number of subscribers"""
 import requests
+
+
 def number_of_subscribers(subreddit):
     headers = {'User-Agent': 'ned6'}
-    res = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit), headers=headers)
+    res = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit),
+        headers=headers)
     if res.status_code != 200:
         return 0
     info = res.json()
